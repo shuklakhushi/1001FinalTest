8ccc8530138d0ee36dacbabdc31cf56a6cda9b72
khushishukla <shuklakhushi3110>
2023-08-18 04:08:39 -0400

    added item model and connected database to firebase and firefoo

---
+++ MDEV1001-FinalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDEV1001-FinalTest
+//
+//  Created by Khushi Shukla on 2023-08-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
